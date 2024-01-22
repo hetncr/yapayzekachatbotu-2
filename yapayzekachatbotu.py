6e17f4e3e95be6c7b1e42d3c2c3cd0741cf04a2b
hetncr <97675352+hetncr@users.noreply.github.com>
2024-01-23 01:15:42 +0300

    Update yapayzekachatbotu.py

--- yapayzekachatbotu.py
+++ yapayzekachatbotu.py
@@ -8,7 +8,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain import PromptTemplate
+#from langchain import PromptTemplate
 from langchain.prompts import PromptTemplate
 from langchain.chains.question_answering import load_qa_chain
 #from langchain.vectorstores import Chroma
